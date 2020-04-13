e0f798ff8c1ccd198bde42a879df42681919f8c2
dennismoha <dennismoha2@gmail.com>
2020-04-13 20:17:06 +0300

    solving logic errors

--- src/estimator.js
+++ src/estimator.js
@@ -2,19 +2,19 @@
 const factor = (data) => {
     let getfactor;
     if (data.periodType.trim().toLowerCase() === 'days') {
-        getfactor = (data.timeToElapse * 1);
+        getfactor = data.timeToElapse * 1;
     } else if (data.periodType.trim().toLowerCase() === 'weeks') {
-        getfactor = (data.timeToElapse * 7);
+        getfactor = data.timeToElapse * 7;
     } else if (data.periodType.trim().toLowerCase() === 'months') {
-        getfactor = (data.timeToElapse * 30);
+        getfactor = data.timeToElapse * 30;
     } else {
         getfactor = 0;
     }
     return getfactor;
 };
 
-const normalCases = (data) => (data.reportedCases * 10) * (2 ** (Math.trunc(factor(data))));
-const severCases = (data) => (data.reportedCases * 50) * (2 ** (Math.trunc(factor(data) / 3)));
+const normalCases = (data) => (data.reportedCases * 10) * (2 ** (Math.trunc(factor(data)) / 3));
+const severCases = (data) => (data.reportedCases * 50) * (2 ** (Math.trunc(factor(data)) / 3));
 // const hospitalBeds = (data) => (0.35 * data.totalHospitalBeds);
 // const income = (data) => data.region.avgDailyIncomeInUSD;
 // const population = (data) => data.region.avgDailyIncomePopulation;
@@ -23,7 +23,7 @@ const covid19ImpactEstimator = (data) => ({
     data,
     impact: {
         currentlyInfected: data.reportedCases * 10,
-        infectionsByRequestedTime: (data.reportedCases * 10) * (2 ** Math.trunc(factor(data))),
+        infectionsByRequestedTime: (normalCases(data)),
         // infectionsByRequestedTime: (normalCases(data)),
         severeCasesByRequestedTime: (normalCases(data)) * 0.15
         // hospitalBedsByRequestedTime: Math.trunc((hospitalBeds(data))-(0.15 * normalCases(data))),
