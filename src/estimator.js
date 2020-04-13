3afb4e3c7b3d750b4b0b0e968eb0e35729917356
dennismoha <dennismoha2@gmail.com>
2020-04-13 21:49:25 +0300

    solving logic errors

--- src/estimator.js
+++ src/estimator.js
@@ -13,33 +13,32 @@ const factor = (data) => {
     return getfactor;
 };
 
-const normalCases = (data) => (data.reportedCases * 10) * (2 ** (Math.trunc(factor(data) / 3)));
-const severCases = (data) => (data.reportedCases * 50) * (2 ** (Math.trunc(factor(data) / 3)));
-// const hospitalBeds = (data) => (0.35 * data.totalHospitalBeds);
-// const income = (data) => data.region.avgDailyIncomeInUSD;
-// const population = (data) => data.region.avgDailyIncomePopulation;
+const normalCases = (data) => (data.reportedCases * 10) * (2 ** (Math.trunc(factor(data)) / 3));
+const severCases = (data) => (data.reportedCases * 50) * (2 ** (Math.trunc(factor(data)) / 3));
+const hospitalBeds = (data) => (0.35 * data.totalHospitalBeds);
+const income = (data) => data.region.avgDailyIncomeInUSD;
+const population = (data) => data.region.avgDailyIncomePopulation;
 
 const covid19ImpactEstimator = (data) => ({
     data,
     impact: {
         currentlyInfected: data.reportedCases * 10,
         infectionsByRequestedTime: (normalCases(data)),
-        // infectionsByRequestedTime: (normalCases(data)),
-        severeCasesByRequestedTime: 0.15 * (normalCases(data))
-        // hospitalBedsByRequestedTime: Math.trunc((hospitalBeds(data))-(0.15 * normalCases(data))),
-        // casesForICUByRequestedTime: Math.trunc(0.05 * (normalCases(data))),
-        // casesForVentilatorsByRequestedTime: Math.trunc(0.02 * (normalCases(data))),
-        // dollarsInFlight: (normalCases(data)) * income * population * (2 ** factor(data))
+        severeCasesByRequestedTime: 0.15 * (normalCases(data)),
+        hospitalBedsByRequestedTime: Math.trunc((hospitalBeds(data)) - (0.15 * normalCases(data))),
+        casesForICUByRequestedTime: Math.trunc(0.05 * (normalCases(data))),
+        casesForVentilatorsByRequestedTime: Math.trunc(0.02 * (normalCases(data))),
+        dollarsInFlight: (normalCases(data)) * income * population * (2 ** factor(data))
     },
 
     severeImpact: {
         currentlyInfected: data.reportedCases * 50,
         infectionsByRequestedTime: (severCases(data)),
-        severeCasesByRequestedTime: (severCases(data)) * 0.15
-        // hospitalBedsByRequestedTime: Math.trunc((hospitalBeds(data)) -(0.15 * severCases(data))),
-        // casesForICUByRequestedTime: Math.trunc(0.5 * (severCases(data))),
-        // casesForVentilatorsByRequestedTime: Math.trunc(0.2 * (severCases(data))),
-        // dollarsInFlight: (severCases(data)) * income * population * (2 ** factor(data))
+        severeCasesByRequestedTime: (severCases(data)) * 0.15,
+        hospitalBedsByRequestedTime: Math.trunc((hospitalBeds(data)) - (0.15 * severCases(data))),
+        casesForICUByRequestedTime: Math.trunc(0.5 * (severCases(data))),
+        casesForVentilatorsByRequestedTime: Math.trunc(0.2 * (severCases(data))),
+        dollarsInFlight: (severCases(data)) * income * population * (2 ** factor(data))
     }
 
 
