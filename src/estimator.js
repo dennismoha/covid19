2d3edbdaf351d483d07dd9f35a545bb7f711af04
dennismoha <dennismoha2@gmail.com>
2020-04-13 22:14:10 +0300

    solving logic errors

--- src/estimator.js
+++ src/estimator.js
@@ -37,7 +37,7 @@ const covid19ImpactEstimator = (data) => ({
         severeCasesByRequestedTime: (severCases(data)) * 0.15,
         hospitalBedsByRequestedTime: Math.trunc((hospitalBeds(data)) - (0.15 * severCases(data))),
         casesForICUByRequestedTime: Math.trunc(0.5 * (severCases(data))),
-        casesForVentilatorsByRequestedTime: Math.trunc(0.2 * (severCases(data))),
+        casesForVentilatorsByRequestedTime: Math.floor(0.2 * (severCases(data))),
         dollarsInFlight: (severCases(data)) * income * population * (2 ** factor(data))
     }
 
